153b90769661d6ab44af6c17540fc2548ed66502
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-09-25 16:22:44 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan7-Integer/src/latihan/pkg7/integer/PBO110118032Latihan7Integer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg7.integer;
+
+/**
+ *
+ * @author
+ * NAMA  : Guntur Maulana Ibrahim
+ * Kelas : PBO1
+ * NIM   : 10118032
+ * Deskripsi Program : Program ini berisi untuk menampilkan tipe-tipe bilangan bulat
+ */
+public class PBO110118032Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       byte b;
+       short s;
+       int i;
+       long l;
+       b = 120;
+       s = 32767;
+       i = 1_000_000_000;
+       l = 90000000000L;
+       System.out.println("Byte\t : " + b);
+       System.out.println("Short\t : " + s);
+       System.out.println("Int\t : " + i);
+       System.out.println("Long\t : " + l);
+       
+       
+        
+       
+       
+    }
+    
+}
